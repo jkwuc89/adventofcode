db302c94a088e75c7f132c8f5b9e48f6f066129c
Keith Wedinger <keith.wedinger@improving.com>
2017-12-03 17:42:01 -0500

    Advent of Code 2017 Day 1 complete

--- src/main/kotlin/com/keithwedinger/Day1.kt
+++ src/main/kotlin/com/keithwedinger/Day1.kt
@@ -1,6 +1,7 @@
 package com.keithwedinger
 
 import org.apache.commons.lang3.StringUtils
+import java.lang.Character.getNumericValue
 
 /**
  * Day 1 Puzzle
@@ -10,14 +11,52 @@ import org.apache.commons.lang3.StringUtils
  */
 fun main(args: Array<String>) {
     val day1 = Day1()
-    println("Sum = " + day1.sum(""))
+    println("sumUsingNextDigit = " + day1.sumUsingNextDigit("1122"))
+    println("sumUsingDigitHalfwayForward = " + day1.sumUsingDigitHalfwayForward("1212"))
 }
 
 class Day1 {
-    fun sum(input: String) : Long {
+    fun sumUsingNextDigit(input: String): Long {
         if (input.isEmpty() || !StringUtils.isNumeric(input)) {
             throw IllegalArgumentException("'$input' is empty or non-numeric")
         }
-        return 0
+
+        val inputArray = input.toCharArray()
+        var sum = 0L
+
+        for (i in 0..inputArray.size - 2) {
+            val currentNumber = getNumericValue(inputArray[i])
+            val nextNumber = getNumericValue(inputArray[i + 1])
+            if (currentNumber == nextNumber) sum += currentNumber
+        }
+
+        val firstNumber = getNumericValue(inputArray.first())
+        val lastNumber = getNumericValue(inputArray.last())
+        if (firstNumber == lastNumber) sum += firstNumber
+
+        return sum
+    }
+
+    fun sumUsingDigitHalfwayForward(input: String): Long {
+        if (input.isEmpty() || !StringUtils.isNumeric(input)) {
+            throw IllegalArgumentException("'$input' is empty or non-numeric")
+        }
+
+        val inputArray = input.toCharArray()
+        val halfwayIndexIncrement = inputArray.size / 2
+        val lastIndex = inputArray.size - 1
+        var sum = 0L
+
+        for (i in 0..lastIndex) {
+            val currentNumber = getNumericValue(inputArray[i])
+            var halfwayForwardIndex = i + halfwayIndexIncrement
+            if (halfwayForwardIndex > lastIndex) {
+                halfwayForwardIndex -= inputArray.size
+            }
+            val numberHalfwayForward = getNumericValue(inputArray[halfwayForwardIndex])
+            if (currentNumber == numberHalfwayForward) sum += currentNumber
+        }
+
+        return sum
     }
 }
