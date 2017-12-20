f42fcc22e46e3c208bdbf330156d9bf6db35edb6
Keith Wedinger <keith.wedinger@improving.com>
2017-12-20 09:33:41 -0500

    Day 9 cleanup

--- src/main/kotlin/com/keithwedinger/Day9.kt
+++ src/main/kotlin/com/keithwedinger/Day9.kt
@@ -7,53 +7,12 @@ package com.keithwedinger
  * @author Keith Wedinger <br>
  * Created On: 12/19/17
  */
-
-/*
-func part2() -> Int {
-    var answer = 0
-    var garbage = false
-    var depth = 1
-    var prevChar: Character = " "
-
-    for currChar in input {
-
-        switch (prevChar, currChar, garbage) {
-        case ("!", "!", true):
-            prevChar = " "
-            continue
-        case ("!", _, true):
-            break
-        case (_, "<", false):
-            garbage = true
-        case (_, ">", true):
-            garbage = false
-        case (_, "{", false):
-            depth += 1
-        case (_, "}", false):
-            depth -= 1
-        case(_, _, true):
-            if currChar != "!" {
-                answer += 1
-            }
-        default:
-            break
-        }
-
-        prevChar = currChar
-    }
-
-    return answer
-}
- */
-
-
 class Day9 {
     data class StreamState(var prevChar: Char, var currChar: Char, var garbage: Boolean)
 
     fun getGroupScoreAndNonCanceledGarbage(stream: String): Pair<Int, Int> {
         val streamChars = stream.toCharArray()
         val streamState = StreamState(' ', ' ', false)
-        var canceledChar = false
         var depth = 1
         var score = 0
         var garbageCount = 0
